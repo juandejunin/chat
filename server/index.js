e7021d83a3664ff627fc3cc1e20afc1727817044
juandejunin <juandejunin75@gmail.com>
2023-10-16 12:00:45 +0200

    init project

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import logger from 'morgan'
+const port = process.env.PORT ?? 3000
+
+const app = express()
+app.use(logger('dev'))
+app.get('/', (req,res) => {
+
+    res.send('<h1> Esto es un cha... ponele</h1>')
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
